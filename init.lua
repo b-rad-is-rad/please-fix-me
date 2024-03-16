7038e84d3b926f2dd48399fb645a59591d631fc0
Brandon Sucher <brandon.sucher@gmail.com>
2024-03-15 20:56:39 -0600

    fixes errors for unrecognized buffer types, adds multiple highlight term support

--- init.lua
+++ init.lua
@@ -3,36 +3,49 @@ local M = require("config")
 M.highlight = function (buf, filetype, first_line_num, last_line_num)
   -- get the lines from the buffer
   local lines = vim.api.nvim_buf_get_lines(buf, first_line_num, last_line_num, false)
-  local comment_syntax = M.config.lang_comment_syntax[filetype].s
-  local escaped_comment_syntax = ""
 
-  for i=1, #comment_syntax do
-    local c = comment_syntax:sub(i,i)
-    escaped_comment_syntax = escaped_comment_syntax .. "%" .. c
-  end
+  -- FIXME currently only looking at single line comments
+  if filetype and
+     M.config.lang_comment_syntax[filetype] and
+     M.config.lang_comment_syntax[filetype].s
+  then
+    local comment_syntax = M.config.lang_comment_syntax[filetype].s
+    local escaped_comment_syntax = ""
 
-  -- search lines
-  for i, line in ipairs(lines) do
-    if line:match("^%s*" .. escaped_comment_syntax) then
-      local j = 1
+    for i=1, #comment_syntax do
+      local c = comment_syntax:sub(i,i)
+      escaped_comment_syntax = escaped_comment_syntax .. "%" .. c
+    end
 
-      -- highlight line
-      while true do
-        local word_start, word_end = string.find(line, "FIXME", j)
+    -- search lines
+    for i, line in ipairs(lines) do
+      -- only matches comments
+      if line:match("^%s*" .. escaped_comment_syntax) then
+        local j = 1
 
-        if word_start and word_end then
-          j = word_end
+        -- TODO this searches the line fully for each search_term
+        -- could be optimized with multi-pointer windows to only
+        -- search once
+        -- highlight search terms in line
+        for search_term in pairs(M.config.search_terms) do
+          while true do
+            local word_start, word_end = string.find(line, search_term, j)
 
-          vim.api.nvim_buf_add_highlight(
-            buf,
-            M.hl_ns_id,
-            "FixMe",
-            first_line_num + i - 1,
-            word_start - 1,
-            word_end
-          )
-        else
-          break
+            if word_start and word_end then
+              j = word_end
+
+              vim.api.nvim_buf_add_highlight(
+                buf,
+                M.hl_ns_id,
+                string.lower(search_term),
+                first_line_num + i - 1,
+                word_start - 1,
+                word_end
+              )
+            else
+              break
+            end
+          end
         end
       end
     end
@@ -40,13 +53,12 @@ M.highlight = function (buf, filetype, first_line_num, last_line_num)
 end
 
 M.setup = function ()
-  local ns_id = vim.api.nvim_create_namespace("FixMeHighlighting")
+  local ns_id = vim.api.nvim_create_namespace("please-fix-me")
   vim.api.nvim_set_hl_ns(ns_id)
-  vim.api.nvim_set_hl(ns_id, "FixMe", {
-    fg = "#FFFFFF",
-    bg = "#FF00EB",
-    bold = true
-  })
+
+  for search_term, hl_config in pairs(M.config.search_terms) do
+    vim.api.nvim_set_hl(ns_id, search_term:lower(), hl_config)
+  end
 
   return ns_id
 end
@@ -62,8 +74,8 @@ M.attach = function ()
 end
 
 M.hl_ns_id = M.setup()
-
 -- attach edit listener when we enter a buffer or window
+-- TODO FIXME terminal and telescope buffers cause errors because filetype is nil or something weird
 vim.api.nvim_create_autocmd(
   {"BufEnter", "BufWinEnter", "WinNew"},
   {
